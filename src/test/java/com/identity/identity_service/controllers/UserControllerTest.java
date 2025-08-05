93a110844cace79338a8034b760e9c25ace65f77
nguyenhuyk3 <huykimcuong5@gmail.com>
2025-08-05 16:02:50 +0700

    feature: add Intergration test

---
+++ src/test/java/com/identity/identity_service/controllers/UserControllerTest.java
@@ -0,0 +1,127 @@
+package com.identity.identity_service.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.identity.identity_service.dto.requests.UserCreationRequest;
+import com.identity.identity_service.dto.responses.UserResponse;
+import com.identity.identity_service.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+public class UserControllerTest {
+    static final MySQLContainer MY_SQL_CONTAINER;
+
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
+        MY_SQL_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void configureTestProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> MY_SQL_CONTAINER.getJdbcUrl());
+        registry.add("spring.datasource.username", () -> MY_SQL_CONTAINER.getUsername());
+        registry.add("spring.datasource.password", () -> MY_SQL_CONTAINER.getPassword());
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private UserCreationRequest request;
+    private UserResponse userResponse;
+    private LocalDate dateOfBirth;
+
+    @BeforeEach
+    void initData() {
+        dateOfBirth = LocalDate.of(1990, 1, 1);
+
+        request = UserCreationRequest
+                .builder()
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .password("12345678")
+                .dateOfBirth(dateOfBirth)
+                .build();
+
+        userResponse = UserResponse
+                .builder()
+                .id("cf0600f538b3")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dateOfBirth(dateOfBirth)
+                .build();
+    }
+
+    @Test
+        //
+    void createUser_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        Mockito.when(userService.createNewUser(ArgumentMatchers.any()))
+                .thenReturn(userResponse);
+
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value(1000))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.id")
+                        .value("cf0600f538b3")
+
+                );
+    }
+
+    @Test
+        //
+    void createUser_usernameInvalid_fail() throws Exception {
+        // GIVEN
+        request.setUsername("joh");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value(1003))
+                .andExpect(MockMvcResultMatchers.jsonPath("message")
+                        .value("Username must be at least 4 characters")
+                );
+    }
+}
---
+++ src/test/java/com/identity/identity_service/services/UserServiceTest.java
@@ -0,0 +1,93 @@
+package com.identity.identity_service.services;
+
+import com.identity.identity_service.dto.requests.UserCreationRequest;
+import com.identity.identity_service.dto.responses.UserResponse;
+import com.identity.identity_service.entities.User;
+import com.identity.identity_service.exceptions.AppException;
+import com.identity.identity_service.repositories.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    private UserCreationRequest request;
+    private UserResponse userResponse;
+    private User user;
+    private LocalDate dateOfBirth;
+
+    @BeforeEach
+    void initData(){
+        dateOfBirth = LocalDate.of(1990, 1, 1);
+
+        request = UserCreationRequest
+                .builder()
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .password("12345678")
+                .dateOfBirth(dateOfBirth)
+                .build();
+
+        userResponse = UserResponse
+                .builder()
+                .id("cf0600f538b3")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dateOfBirth(dateOfBirth)
+                .build();
+
+        user = User.builder()
+                .id("cf0600f538b3")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dateOfBirth(dateOfBirth)
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success(){
+        // GIVEN
+        when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        when(userRepository.save(any())).thenReturn(user);
+
+        // WHEN
+        var response = userService.createNewUser(request);
+        // THEN
+
+        Assertions.assertThat(response.getId()).isEqualTo("cf0600f538b3");
+        Assertions.assertThat(response.getUsername()).isEqualTo("john");
+    }
+
+    @Test
+    void createUser_userExisted_fail(){
+        // GIVEN
+        when(userRepository.existsByUsername(anyString())).thenReturn(true);
+
+        // WHEN
+        var exception = assertThrows(AppException.class,
+                () -> userService.createNewUser(request));
+
+        // THEN
+        Assertions.assertThat(exception.getErrorCode().getCode())
+                .isEqualTo(1002);
+    }
+}
